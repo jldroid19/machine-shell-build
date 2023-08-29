c7731116117c7cc602c36c77e56d71e11327ad76
jldroid19 <jake.lafrance19@gmail.com>
2023-08-28 20:37:41 -0400

    Jenkins piepline update

--- build_tag_push.sh
+++ build_tag_push.sh
@@ -7,10 +7,10 @@ echo "Building new image"
 docker build -t machine-book:latest .
 
 # echo "Login to Digital Ocean Registry"
-doctl registry login
+# doctl registry login
 
-# echo "Tagging Site image"
-docker tag machine-book:latest registry.digitalocean.com/sandboxcsp/machine-book:latest
+# # echo "Tagging Site image"
+# docker tag machine-book:latest registry.digitalocean.com/sandboxcsp/machine-book:latest
 
-# echo 'Pushing container to Digital Ocean Repo'
-docker push registry.digitalocean.com/sandboxcsp/machine-book:latest
+# # echo 'Pushing container to Digital Ocean Repo'
+# docker push registry.digitalocean.com/sandboxcsp/machine-book:latest
